ab0c02fd18a856bd9cc4765ba1bf62192021bbe0
JP <jphpsf@gmail.com>
2011-05-23 13:24:13 -0700

    Do not restart charts for each page load

--- pan-prototype.js
+++ pan-prototype.js
@@ -1,35 +1,35 @@
 (function( $, undefined ) {
 
 	/* update device page title when clicking on a device menu */
-
 	$('a.device-link').live('click',function(e){
 		$('#device-name').text($(e.target).text());
 	});
 
+	var charts={},
+		initChart= function(id) {
+			if (charts[id]) {
+				return false;
+			}
+			var dataSet = new TimeSeries();
+			setInterval(function() {
+			var now = new Date().getTime();
+			dataSet.append(now, Math.random());
+			}, 1000);
+			// Build the timeline
+			var smoothie = new SmoothieChart({ millisPerPixel: 20, grid: { strokeStyle: '#555555', lineWidth: 1, millisPerLine: 1000, verticalSections: 4 }});
+			smoothie.addTimeSeries(dataSet, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.2)', lineWidth: 3 });
+			smoothie.streamTo(document.getElementById(id), 1000);
+			charts[id]=true;
+		}
+
 	/* start updating the current chart */
 	$('#current').live('pageshow',function(e){
-      var dataSet = new TimeSeries();
-      setInterval(function() {
-        var now = new Date().getTime();
-        dataSet.append(now, Math.random());
-      }, 1000);
-      // Build the timeline
-      var smoothie = new SmoothieChart({ millisPerPixel: 20, grid: { strokeStyle: '#555555', lineWidth: 1, millisPerLine: 1000, verticalSections: 4 }});
-      smoothie.addTimeSeries(dataSet, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.2)', lineWidth: 3 });
-      smoothie.streamTo(document.getElementById('current-chart'), 1000);
+		initChart('current-chart');
 	});
 
 	/* start updating the device chart */
 	$('#device').live('pageshow',function(e){
-      var dataSet = new TimeSeries();
-      setInterval(function() {
-        var now = new Date().getTime();
-        dataSet.append(now, Math.random());
-      }, 1000);
-      // Build the timeline
-      var smoothie = new SmoothieChart({ millisPerPixel: 20, grid: { strokeStyle: '#555555', lineWidth: 1, millisPerLine: 1000, verticalSections: 4 }});
-      smoothie.addTimeSeries(dataSet, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.2)', lineWidth: 3 });
-      smoothie.streamTo(document.getElementById('device-chart'), 1000);
+		initChart('device-chart');
 	});
 
 	/* theme switcher in options */
